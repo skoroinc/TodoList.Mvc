46b10c0d5bc42f18b8396006dc5b12eca937756e
Rusichbtw <104261346+skoroinc@users.noreply.github.com>
2025-01-28 02:22:57 +0300

    add view
    -

---
+++ WebApplication1/Views/Shared/Components/TaskItem/TaskItemViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Mvc.Views.Shared.Components.TaskItem
+{
+    public class TaskItemViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(TaskApp task)
+        {
+            return View(task);
+        }
+    }
+}
